417a9798ea8e1a1a7158f6fa81246b89cccf1db2
rquinterosv <rafael.quinterosv@gmail.com>
2023-05-01 23:10:18 -0400

    adding pages and components folders

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is my component.</p>
+    </div>
+  );
+}
+
+export default Home;
